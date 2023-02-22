380ba02405a4e6abb600d96941e553d17058d20f
Michael Lange <dingoeatingfuzz@gmail.com>
2023-02-22 13:41:56 -0800

    Read a color from ENV

--- crates/nogo-draw/src/main.rs
+++ crates/nogo-draw/src/main.rs
@@ -1,4 +1,7 @@
+use nannou::color::*;
 use nannou::prelude::*;
+use regex::Regex;
+use std::env;
 
 fn main() {
     nannou::app(model).simple_window(view).run();
@@ -11,15 +14,16 @@ fn model(_app: &App) -> Model {
 }
 
 fn view(app: &App, _model: &Model, frame: Frame) {
+    let line_color = color_from_env("NOGO_COLOR");
     let draw = app.draw();
-    draw.background().color(PURPLE);
-
     let boundary = app.window_rect();
+
+    draw.background().color(PURPLE);
     draw.line()
         .start(pt2(-boundary.w() / 2.0, boundary.h() / 2.0))
         .end(pt2(boundary.w() / 2.0, -boundary.h() / 2.0))
         .weight(4.0)
-        .color(WHITE);
+        .color(line_color);
 
     draw.to_frame(app, &frame).unwrap();
 
@@ -28,5 +32,24 @@ fn view(app: &App, _model: &Model, frame: Frame) {
         .expect("no project_path?")
         .join(app.exe_name().unwrap())
         .with_extension("png");
-    app.main_window().capture_frame(path);
+
+    app.main_window().capture_frame(path)
+}
+
+fn color_from_env(env_var: &str) -> Rgba<u8> {
+    let value = env::var(env_var).unwrap_or("#000000FF".to_string());
+    let re = Regex::new(r"^#[\dA-Fa-f]{8}$").unwrap();
+    if !re.is_match(&value) {
+        panic!("Invalid color '{}' found for ENV VAR {}", value, env_var);
+    }
+
+    rgba_u32(u32::from_str_radix(value.trim_start_matches("#"), 16).unwrap())
+}
+
+fn rgba_u32(hex: u32) -> Rgba<u8> {
+    let alpha: u8 = (hex & 0xFF) as u8;
+    let blue: u8 = ((hex >> 8) & 0xFF) as u8;
+    let green: u8 = ((hex >> 16) & 0xFF) as u8;
+    let red: u8 = ((hex >> 24) & 0xFF) as u8;
+    rgba(red, green, blue, alpha)
 }
